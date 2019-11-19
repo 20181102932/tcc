cf9c5727dee6c6813e83e834b16e763020dbd067
20181102932 <908907990@qq.com>
2019-11-19 15:54:46 +0800

    Initial Commit

---
+++ tcc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  tcc
+//
+//  Created by s20181102932 on 2019/11/19.
+//  Copyright © 2019 wjx. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
