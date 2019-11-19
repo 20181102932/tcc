435757686b31a3603ff7612ef29c059ae122b551
20181102932 <908907990@qq.com>
2019-11-19 17:16:11 +0800

    123

--- tcc/main.c
+++ tcc/main.c
@@ -7,9 +7,55 @@
 //
 
 #include <stdio.h>
-
+#include<stdbool.h>
+struct car{
+    char ad;
+    int id;
+    int atime;
+    int dtime;
+};
+struct SNode{
+    struct car * a;
+    int top;
+    int maxsize;
+};
+struct SNode * creatpark(int maxsize)
+{
+    struct SNode * S = (struct SNode *)malloc(sizeof(struct SNode));
+    S->a=(struct car *)malloc(maxsize * sizeof(struct car)); //?
+    S->top=-1;
+    S->maxsize=maxsize;
+    return S;
+}
+bool IsFull(struct SNode * S)
+{
+    return(S->top==S->maxsize-1);
+}
+bool Push(struct SNode * S,struct car a)
+{
+    if(IsFull(S)){
+        printf("停车场已满");
+        return false;
+    }
+    else {
+        S->a[++(S->top)]=a;
+        return false;
+    }
+}
+bool IsEmpty(struct SNode * S)
+{
+    return(S->top==-1);
+}
+struct car pop(struct SNode * S)
+{
+    if(IsEmpty(S)){
+        printf("停车场为空");
+        //return;
+    }
+    else
+        return(S->a[S->top--]);
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
     return 0;
 }
